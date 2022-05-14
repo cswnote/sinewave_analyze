37315aeed0a4752e2921c726bfe37da46a6dfd84
cswnote <cswnote@gmail.com>
2022-05-14 15:04:04 +0900

    freq commit

--- temp.py
+++ temp.py
@@ -1,15 +1,28 @@
-import openpyxl
-import pandas as pd
-import numpy as np
 import os
+import sys
+import pyautogui as pag
+import time
+import openpyxl
+import FILE_MANAGEMENT
+from datetime import datetime
+# import keyboard # homebrew에서 설치되었을 때는 되었는데 지금은 안됨
 
-path = 'D:/work/data_analyze/'
+path = os.getcwd() + '/Evaluation/Osciloscope/'
 csv_path = path + 'csv/'
+# path = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/autotest/Evaluation'
+filelist = os.listdir(csv_path)
 
-filename = 'file name.xlsx'
 
-wb = openpyxl.load_workbook(path + filename, data_only=True)
 
-sheet_list = wb.get_sheet_names()
-w1 = wb[sheet_list[0]]
-print(w1)
+for file in filelist:
+    if file[:3] == 'tek':
+        a.append(file.split('tek')[1][:4])
+
+
+filelist = [int(file.split('tek')[1].split('_kmonCap')[0]) for file in filelist if file[:3] == 'tek']
+
+for file in filelist:
+    src = os.path.join(path, file + '.png')
+    dst = file + '_kmonCap.png'
+    dst = os.path.join(path, dst)
+    os.rename(src, dst)
