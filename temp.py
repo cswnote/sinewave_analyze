b5e0841373779f53e1501715d8914f049065a3cc
cswnote <103407371+cswnote@users.noreply.github.com>
2022-05-24 08:57:54 +0900

    Update temp.py
    from office desk

--- temp.py
+++ temp.py
@@ -1,14 +1,14 @@
 import openpyxl
 import os
-import pandas as pd
 
-mac_m1 = True
+path = 'D:/work/data_analyze/excel/'
+file = 'summary.xlsx'
 
-if mac_m1:
-    path = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/sinewave_analyze/Evaluation/sample/'
-    path_csv = path + 'csv/'
-    path_excel = path + 'excel/'
-    path_summary = path + 'summary/'
-    path_information = path + 'test infomation/'
-    path_kmon = path + 'kmon_csv'
+wb = openpyxl.load_workbook(path + file)
+ws = wb.active
 
+sheets = wb.get_sheet_names()
+
+name = ws.title
+
+print(sheets)
