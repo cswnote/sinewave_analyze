1179d196134f3886f8aa880e1e8008a8f1ba83e4
cswnote <cswnote@gmail.com>
2022-06-18 08:07:05 +0900

    from desktop

--- temp3.py
+++ temp3.py
@@ -1,27 +1,24 @@
-# import os
-# import openpyxl
-#
-# path = 'D:/winston/OneDrive - (주)필드큐어/data_analyze/'
-# path_csv = path + 'tek_csv/'
-# path_excel = path + 'tek_excel/'
-# path_summary = path + 'summary/'
-# path_information = path + 'test information/'
-# path_kmon = path + 'kmon_csv/'
-#
-# files = os.listdir(path_kmon)
-# # files = [file for file in files if file[:3] == 'tek']
-#
-# for file in files:
-#     scr = path_kmon + file
-#     file = file.split('_')[0] + '_kmon_' + file[10:]
-#     dst = path_kmon + file
-#     os.rename(scr, dst)
+import os
+import openpyxl
+
+path = 'D:/data_analyze/'
+path_csv = path + 'tek_csv/'
+path_excel = path + 'tek_excel/'
+path_summary = path + 'summary/'
+path_information = path + 'test information/'
+path_kmon = path + 'kmon_csv/'
+
+files = os.listdir(path_excel)
+files.sort()
+files = [file for file in files if file.endswith('.xlsx')]
+
+previous = 5173
+abssent = []
+for file in files:
+    if previous - int(file.split('.xlsx')[0][3:7]) != -1:
+        abssent.append(file)
+    previous = int(file.split('.xlsx')[0][3:7])
+
+print(abssent)
 
 
-a = ['Volt', 'Curr']
-
-for item in a:
-    print(item)
-
-a = [item.lower() for item in a if True]
-print(a)
