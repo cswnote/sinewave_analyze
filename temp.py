03a6aba041f00e039cf6a5bae1594e43225153ac
cswnote <cswnote@gmail.com>
2022-07-22 08:47:52 +0900

    from desktop

--- temp.py
+++ temp.py
@@ -2,21 +2,19 @@ import pandas as pd
 import openpyxl
 import os
 
-path = '/Volumes/16G/'
+path = 'D:/data_analyze/tek_excel/'
 
 files = os.listdir(path)
 files = [file for file in files if file[:3] == 'tek']
 files.sort()
 
-base_num = -1776
 for file in files:
-    scr = path + file
-    extenstion = file.split('.')[1]
-    filenum = file.split('tek')[1] .split('.')[0]
-
-    filenum = int(filenum) + base_num
-    filenum = '{:05d}'.format(filenum)
-
-    dst = path + 'tek' + filenum + '.' + extenstion
-
-    os.rename(scr, dst)
+    src = path + file
+    flist = file.split(' ')
+    flist[2] = '300ohm'
+    file =''
+    for i in flist:
+        file = file + i + ' '
+    file = file.strip()
+    dst = path + file
+    os.rename(src, dst)
