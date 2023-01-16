73016e95eda27c2e87a9d9a896ab35463da7748f
cswnote <cswnote@gmail.com>
2023-01-16 17:00:05 +0900

    from M1 Macbook Air

--- kmon_value_correction.py
+++ kmon_value_correction.py
@@ -2,30 +2,45 @@ import pandas as pd
 import numpy as np
 import sys
 import os
+import platform
+import gc
 
-path = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/sinewave_analyze/Evaluation/summary/'
-file = 'summary 08Ch3 phantom I sweep.xlsx'
+if __name__ == '__main__':
+    if platform.platform() == 'macOS-13.1-arm64-arm-64bit':
+        path = '/Volumes/Winston 2T/work/fieldcure/multi channel test data/'
+    elif platform.platform() == 'Windows-10-10.0.19044-SP0':
+        path = ''
 
-sheets = pd.ExcelFile(path + file).sheet_names
-df_origin = pd.read_excel(path + file, sheets[-1])
-columns = [col for col in df_origin.columns if ('RF Volt' in col or 'Pwm Ch' in col) and 'deviation' not in col]
+    path_summary = path + 'summary/'
 
-df = df_origin.copy()
-for col in columns:
-    for i in range(len(df)):
-        if 'Volt' in col:
-            df.at[i, col] = df.at[i, col] * 0.1
-        elif 'Pwm Ch' in col:
-            df.at[i, col] = df.at[i, col] * 10
+    df_ctrl = pd.read_excel(path + 'eval_control.xlsx', sheet_name='kmon digit change set RFamp')
+    files = list(df_ctrl['File'].dropna())
+    df_ctrl = df_ctrl[['Name', 'digit']]
+    df_ctrl = df_ctrl.dropna()
 
-sheet_name = 'with kmon correction'
-if not os.path.exists(path):
-    with pd.ExcelWriter(path + file, mode='w', engine='openpyxl') as writer:
-        df.to_excel(writer, sheet_name, index=False)
-        print('create sheet: ', sheet_name)
-else:
-    with pd.ExcelWriter(path + file, mode='a', engine='openpyxl') as writer:
-        try:
-            df.to_excel(writer, sheet_name, index=False)
-        except:
-            print('그 시트 있다 안카나: ', sheet_name)
+    for file in files:
+        sheets = pd.ExcelFile(path_summary + file).sheet_names
+        df = pd.read_excel(path_summary + file, sheet_name=sheets[-1])
+        df_origin = df.copy()
+        for i in range(len(df)):
+            for j in range(len(df_ctrl)):
+                if df_ctrl.at[j, 'digit'].split(' ')[0] == 'reduce':
+                    df.at[i, df_ctrl.at[j, 'Name']] = df.at[i, df_ctrl.at[j, 'Name']] * int(df_ctrl.at[j, 'digit'].split(' ')[1])  * 0.1
+                elif df_ctrl.at[j, 'digit'].split(' ')[0] == 'raise':
+                    df.at[i, df_ctrl.at[j, 'Name']] = df.at[i, df_ctrl.at[j, 'Name']] * int(df_ctrl.at[j, 'digit'].split(' ')[1]) * 10
+
+        filename = path_summary + file
+        sheet = sheets[-1] + ' correction'
+        df_write = df
+        index_ = False
+        if not os.path.exists(filename):  # excel.path로 변경
+            with pd.ExcelWriter(filename, mode='w', engine='openpyxl') as writer:
+                df_write.to_excel(writer, sheet_name=sheet, index=index_)
+        else:
+            with pd.ExcelWriter(filename, mode='a', engine='openpyxl') as writer:
+                df_write.to_excel(writer, sheet_name=sheet, index=index_)
+
+        del df_write, index_, sheet, filename
+        gc.collect()
+
+    print('===================')
--- temp5.py
+++ temp5.py
@@ -1,51 +1,166 @@
-import os
 import pandas as pd
 import numpy as np
+import sys
+import os
+import platform
+import gc
+from collections import defaultdict
 
-path = 'C:/data_analyze/tek_excel/'
+if platform.platform() == 'macOS-13.1-arm64-arm-64bit':
+    path = '/Volumes/Winston 2T/work/fieldcure/multi channel test data/'
+elif platform.platform() == 'Windows-10-10.0.19044-SP0':
+    path = ''
 
-files = os.listdir(path)
-files = [file for file in files if len(file)==21]
-#
-df_ch3 = pd.DataFrame()
-df_ch4 = pd.DataFrame()
+path_summary = path + 'summary/'
 
-for file in files:
-    if file[11:-5] == '08Ch3':
-        if len(df_ch3) != 0:
-            df = pd.read_excel(path + file)
-            df_ch3 = pd.concat([df_ch3, df])
-        else:
-            df_ch3 = pd.read_excel(path + file)
-    elif file[11:-5] == '08Ch4':
-        if len(df_ch4) != 0:
-            df = pd.read_excel(path + file)
-            df_ch4 = pd.concat([df_ch4, df])
-        else:
-            df_ch4 = pd.read_excel(path + file)
+'''  data dictionary 구조
+     data['phantom type']['test code']['측정 channel']['측정조건'] = df
+     data['R']['channel 저항']['interference 저항']['측정 channel']['측정조건'] = df
+'''
+data_phantom = {'2ch_gel': {'230106_1': {'08ch3': {}, '08ch4':{}}}}
+data_R_model = {'4ch_R150': {'R150': {'08ch3': {}, '08ch4': {}}}}
+
+files = os.listdir(path_summary)
+files = [file for file in files if '08 12 08' in file and '._' not in file]
+
+df_all_ch3 = pd.read_excel(path_summary + files[0], sheet_name='with kmon correction')
+df_all_ch4 = pd.read_excel(path_summary + files[1], sheet_name='with kmon correction')
+
+start = 0
+stop = 251
+test_item = 'ch3_curr_sweep_ch4_220ma'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_phantom['2ch_gel']['230106_1']['08ch3'][test_item] = df_ch3
+data_phantom['2ch_gel']['230106_1']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 251
+test_item = 'ch4_curr_sweep_ch3_220ma'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_phantom['2ch_gel']['230106_1']['08ch3'][test_item] = df_ch3
+data_phantom['2ch_gel']['230106_1']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 75
+test_item = 'ch3_volt_sweep_ch4_50vp'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_phantom['2ch_gel'] = {'230109_1': {}}
+data_phantom['2ch_gel']['230109_1'] = {'08ch3': {}, '08ch4': {}}
+data_phantom['2ch_gel']['230109_1']['08ch3'][test_item] = df_ch3
+data_phantom['2ch_gel']['230109_1']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 75
+test_item = 'ch4_volt_sweep_ch3_50vp'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_phantom['2ch_gel']['230109_1']['08ch3'][test_item] = df_ch3
+data_phantom['2ch_gel']['230109_1']['08ch4'][test_item] = df_ch4
+
+start = 652
+stop = 903
+test_item = 'ch3_curr_sweep_all_220ma'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['4ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['4ch_R150']['R150']['08ch4'][test_item] = df_ch4
+
+start = 903
+stop = 1154
+test_item = 'ch4_curr_sweep_all_220ma'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['4ch_R150']['R150']['08ch3'][test_item] = df_ch3#_ch4_volt_sweep_ch3_50vp
+data_R_model['4ch_R150']['R150']['08ch4'][test_item] = df_ch4#_ch4_volt_sweep_ch3_50vp
+
+start = 1154
+stop = start + 51
+test_item = 'ch3_volt_sweep_all_25vp'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['4ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['4ch_R150']['R150']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 51
+test_item = 'ch4_volt_sweep_all_25vp'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['4ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['4ch_R150']['R150']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 251
+test_item = 'ch3_curr_sweep_ch4_220ma'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['2ch_R150'] = {'R150': {'08ch3': {}, '08ch4': {}}}
+data_R_model['2ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['2ch_R150']['R150']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 251
+test_item = 'ch4_curr_sweep_ch3_220ma'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['2ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['2ch_R150']['R150']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 50
+test_item = 'ch3_volt_sweep_ch4_25vp'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['2ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['2ch_R150']['R150']['08ch4'][test_item] = df_ch4
+
+start = stop
+stop = stop + 50
+test_item = 'ch4_volt_sweep_ch3_25vp'
+print("df_ch3 {} ~ {}".format(df_all_ch3.at[start, 'filename'], df_all_ch3.at[stop - 1, 'filename']))
+print("df_ch4 {} ~ {}".format(df_all_ch4.at[start, 'filename'], df_all_ch4.at[stop - 1, 'filename']))
+print(test_item)
+df_ch3 = df_all_ch3.iloc[start:stop, :]
+df_ch4 = df_all_ch4.iloc[start:stop, :]
+data_R_model['2ch_R150']['R150']['08ch3'][test_item] = df_ch3
+data_R_model['2ch_R150']['R150']['08ch4'][test_item] = df_ch4
 
 
-df_summary = df_ch3
-filename = 'summary 08 12 08Ch3.xlsx'
-sheet = 'summary'
-if not os.path.exists(path + filename):
-    with pd.ExcelWriter(path + filename, mode='w', engine='openpyxl') as writer:
-        df_summary.to_excel(writer, sheet_name=sheet, index=False)
-else:
-    with pd.ExcelWriter(path + filename, mode='a', engine='openpyxl') as writer:
-        df_summary.to_excel(writer, sheet_name=sheet, index=False)
 
-
-df_summary = df_ch4
-filename = 'summary 08 12 08Ch4.xlsx'
-sheet ='summary'
-if not os.path.exists(path + filename):
-    with pd.ExcelWriter(path + filename, mode='w', engine='openpyxl') as writer:
-        df_summary.to_excel(writer, sheet_name=sheet, index=False)
-else:
-    with pd.ExcelWriter(path + filename, mode='a', engine='openpyxl') as writer:
-        df_summary.to_excel(writer, sheet_name=sheet, index=False)
-
-
-print('===============================')
-
+print('==================')
